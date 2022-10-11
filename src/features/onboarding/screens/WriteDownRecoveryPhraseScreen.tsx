9585e5a4df8da8e1f1153ee932bfd4c4d2dcd521
Allen <allenmuhani@gmail.com>
2022-10-11 11:10:00 +0300

    created write down recovery phrase screen

--- src/features/onboarding/screens/WriteDownRecoveryPhraseScreen.tsx
+++ src/features/onboarding/screens/WriteDownRecoveryPhraseScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import {StyleSheet, TextInput, ToastAndroid, View} from 'react-native';
 import {connect} from 'react-redux';
 import {RootState} from '../../../app-redux-store/store';
 import {AppColors} from '../../../ui_lib_configs/colors';
@@ -11,6 +11,7 @@ import {Button, Switch, Text} from 'react-native-ui-lib';
 import Screen from '../../../app_components/Screen';
 import {FONTS} from '../../../ui_lib_configs/fonts';
 import {useNavigation} from '@react-navigation/native';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 /**
  * Contains the onboarding UI.
@@ -20,6 +21,17 @@ export const TermsAndConditions = () => {
   const [seedPhrase, setSeedPhrase] = useState(
     'Horse  giraffe  dog money  book  fire  drink cup  phone  car  jacket computer  wire  charger curtain  router  window  plate  floor  key  wine glass  oak  watch',
   );
+  const [writtenDownSeedPhrase, setWrittenDownSeedPhrase] = useState(false);
+
+  const copySeedPhraseToClipBoard = () => {
+    Clipboard.setString(seedPhrase);
+    ToastAndroid.showWithGravity(
+      'Copied seedphrase.',
+      ToastAndroid.SHORT,
+      ToastAndroid.TOP,
+    );
+  };
+
   return (
     <Screen style={style.rootComponent}>
       <View style={style.container}>
@@ -28,31 +40,57 @@ export const TermsAndConditions = () => {
           <Text color={AppColors.light_green} displayBold>
             Write down recovery phrase
           </Text>
-          <Text color={AppColors.black} body3>
+          <Text color={AppColors.black} body2>
             Here is your recovery phrase. Write it down and store in safe place.
             Do not save it in your phone or your email.
           </Text>
         </View>
-
         {/* Body text group section. */}
-        <View style={style.textGroup}>
+        <View
+          style={style.textGroup}
+          onTouchEnd={() => {
+            copySeedPhraseToClipBoard();
+          }}>
           <TextInput
             editable={false}
             multiline
             numberOfLines={4}
             value={seedPhrase}
-            color={AppColors.green}
+            color={AppColors.black}
+            style={{
+              ...FONTS.body1,
+            }}
+            selectable={true}
           />
         </View>
 
-        <Switch onColor={AppColors.light_green}></Switch>
+        <View style={style.switchContainer}>
+          <Switch
+            disabled={false}
+            onColor={AppColors.light_green}
+            value={writtenDownSeedPhrase}
+            onValueChange={() => {
+              setWrittenDownSeedPhrase(!writtenDownSeedPhrase);
+            }}
+          />
+          <Text
+            style={[
+              style.switchLabel,
+              writtenDownSeedPhrase
+                ? {color: AppColors.black}
+                : {color: AppColors.brown},
+            ]}>
+            Yes, I have written down my phrase
+          </Text>
+        </View>
+
         {/* Button group section. */}
         <View style={style.buttonGroup}>
           <Button
             style={style.button}
             outline={true}
             outlineColor={AppColors.light_green}
-            label={'Accept'}
+            label={'Continue'}
             secondary
             labelStyle={{
               ...FONTS.h4,
@@ -60,6 +98,7 @@ export const TermsAndConditions = () => {
             onPress={() => {
               navigation.navigate('EnterUserName');
             }}
+            disabled={!writtenDownSeedPhrase}
           />
         </View>
       </View>
@@ -103,8 +142,16 @@ const style = StyleSheet.create({
     justifyContent: 'space-around',
   },
   textGroup: {
-    flex: 0.4,
     justifyContent: 'space-around',
-    paddingTop: hp('4%'),
+    paddingHorizontal: wp('5%'),
+    paddingVertical: hp('1%'),
+    paddingBottom: hp('2%'),
+    backgroundColor: '#ffff',
+    borderRadius: wp('5%'),
+  },
+  switchLabel: {...FONTS.body5, marginLeft: wp('4%')},
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
