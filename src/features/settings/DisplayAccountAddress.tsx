cb528bca9c01d678e2c0cab7147879d96bcef19f
Allen <allenmuhani@gmail.com>
2022-10-27 16:31:58 +0300

    added QR code screen

--- src/features/settings/DisplayAccountAddress.tsx
+++ src/features/settings/DisplayAccountAddress.tsx
@@ -1,7 +1,10 @@
 import React, {useEffect} from 'react';
 import Screen from '../../app_components/Screen';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import QRCode from 'react-native-qrcode-svg';
+import {selectPublicAddress} from '../onboarding/redux_store/selectors';
+import {useSelector} from 'react-redux';
 
 const DisplayAccountAddress = () => {
   const navigation = useNavigation();
@@ -9,6 +12,8 @@ const DisplayAccountAddress = () => {
   useEffect(() => {
     navigation.getParent()?.setOptions({headerShown: false});
     navigation.setOptions({title: 'Account Address', headerTransparent: true});
+    // const publicAddress = useSelector(selectPublicAddress);
+
     return () => {
       navigation.getParent()?.setOptions({headerShown: true});
     };
@@ -17,6 +22,10 @@ const DisplayAccountAddress = () => {
   return (
     <Screen>
       <Text>Display account address</Text>
+      <QRCode value="http://awesome.link.qr" size={150} />
+      <View>
+        <Text></Text>
+      </View>
     </Screen>
   );
 };
